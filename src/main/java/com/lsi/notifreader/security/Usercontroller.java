e8e84c0f91f25f2660d84846e7ba8bc5c92e4aa6
issam-sudo <57816123+issam-sudo@users.noreply.github.com>
2020-01-15 02:20:44 +0100

    ss

---
+++ src/main/java/com/lsi/notifreader/security/Usercontroller.java
@@ -0,0 +1,18 @@
+package com.lsi.notifreader.security;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class Usercontroller {
+
+	@GetMapping
+	public Principal getUser(Principal user) {
+		return user;
+	}
+
+}
